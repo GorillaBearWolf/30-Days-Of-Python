e88db07acb8d03f282847e3af3e3bae2d9cf1a79
Matthew Refsnider <mrefsnider@icloud.com>
2023-07-24 12:44:42 -0400

    wip day 2

---
+++ _day-2/variables.py
@@ -0,0 +1,42 @@
+﻿# Day 2: 30 Days of python programming
+
+first_name = "Gorilla"
+last_name = "BearWolf"
+full_name = "GorillaBearWolf"
+country = None
+city = None
+age = 3.14
+year = 0
+is_married = False
+is_true = True
+is_light_on = True
+a, b, c = 1, 2, 3
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(len(first_name))
+
+print((len(first_name)) == (len(last_name)))
+
+num_one, num_two = 5, 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
